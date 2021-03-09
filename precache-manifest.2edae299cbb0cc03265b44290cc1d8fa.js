62a41db25683192ee29f66b04d4ed1e434250244
dtmhdev89 <dinh.tran.minh.hieu@sun-asterisk.com>
2021-03-09 14:35:17 +0700

    Updates

---
+++ precache-manifest.2edae299cbb0cc03265b44290cc1d8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ce0b2d63a3b87c532d60c93310d586",
+    "url": "/gitio.react-redux-essential-rutorial/index.html"
+  },
+  {
+    "revision": "bc42c9a5b7f40c5ea1ff",
+    "url": "/gitio.react-redux-essential-rutorial/static/css/main.b0211d57.chunk.css"
+  },
+  {
+    "revision": "e4c94020a627ace461f8",
+    "url": "/gitio.react-redux-essential-rutorial/static/js/2.00c24c2c.chunk.js"
+  },
+  {
+    "revision": "3f598473a645111fe9a1b1e43cd3e4b8",
+    "url": "/gitio.react-redux-essential-rutorial/static/js/2.00c24c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc42c9a5b7f40c5ea1ff",
+    "url": "/gitio.react-redux-essential-rutorial/static/js/main.86fa737a.chunk.js"
+  },
+  {
+    "revision": "1609c4e898580b7ebfda",
+    "url": "/gitio.react-redux-essential-rutorial/static/js/runtime-main.1907fb62.js"
+  }
+]);
